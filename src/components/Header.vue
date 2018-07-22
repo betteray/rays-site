21d4448c8d3ef2f592d01c2e436d9a63f8791f1f
ray <ray@raydeiMac.lan>
2018-07-22 23:19:00 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="Header">
+        <ul class="h-list">
+            <li>首页</li>
+            <li>归档</li>
+            <li>关于</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+    .Header {
+        font-weight: bold;
+        text-align: center;
+    }
+
+    .h-list li {
+        display: inline;
+        line-height: 40px;
+        margin: 20px;
+    }
+
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <Header/>
+
+</template>
+
+<script>
+    import Header from '../components/Header.vue'
+    export default {
+        name: 'Index',
+        components: {Header},
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
